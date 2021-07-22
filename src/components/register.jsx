6e6489eee05dc20fd5281cd6e354ab432f2bb9e1
Atena Razm <atenarazm@gmail.com>
2021-07-22 13:44:09 -0700

    RegisterForm component got completed with Joi validation and rendering input and button methods

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
-const RegisterForm = () => {
-  return (
-    <div>
-      <h1>Register</h1>
-    </div>
-  );
-};
+class RegisterForm extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().email().label("Username"),
+    password: Joi.string().required().min(5).label("Password"),
+    name: Joi.string().required().label("Name"),
+  };
+
+  doSubmit = () => {
+    // call the server...
+    console.log("submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
 
 export default RegisterForm;
